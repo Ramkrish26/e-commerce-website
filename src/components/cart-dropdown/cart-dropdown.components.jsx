62f7a344a06133567a08d923af10e3503b402f56
Ramkrish26git config user.name Ramkrish26 <ramakrishnan2629@gmail.com>
2023-01-02 22:25:23 +0530

    Adding cart icon and cart dropdown

---
+++ src/components/cart-dropdown/cart-dropdown.components.jsx
@@ -0,0 +1,13 @@
+import '../cart-dropdown/cart-component.styles.scss'
+import Button from '../button/button.component'
+
+const CartDropdown = () => {
+    return (
+        <div className='cart-dropdown-container'>
+            <div className='cart-items' />
+            <Button>CHECKOUT</Button>
+        </div>
+    );
+}
+
+export default CartDropdown;
