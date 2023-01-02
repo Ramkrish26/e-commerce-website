5b4bb36823a394dce2b093d45ce8bb849599d03a
Ramkrish26git config user.name Ramkrish26 <ramakrishnan2629@gmail.com>
2023-01-02 22:05:42 +0530

    Adding shop page

---
+++ src/components/contexts/products.context.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+import SHOP_DATA from '../../shop-data.json'
+
+export const ProductContext = createContext({
+    product: [],
+    setProduct: null,
+})
+
+export const ProductProvider = ({ children }) => {
+    const [product, setProduct] = useState(SHOP_DATA);
+    const value = { product, setProduct }
+
+    return (
+        <ProductContext.Provider value={value}>
+            {children}
+        </ProductContext.Provider>
+    );
+}
