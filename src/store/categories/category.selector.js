30277fc75b5eb3163757349cdf399d6935bbfb90
Ramkrish26git config user.name Ramkrish26 <ramakrishnan2629@gmail.com>
2023-02-05 16:59:58 +0530

    Adding reselect in Redux

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,10 +1,19 @@
-export const selectCategoriesMap = (state) => {
-    const categoriesMap = state.categories.categories.reduce(
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCatgories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+)
+
+export const selectCategoriesMap = createSelector(
+    [selectCatgories],
+    (categories) => categories.reduce(
         (acc, { title, items }) => {
             acc[title.toLowerCase()] = items;
             return acc;
         },
         {}
-    );
-    return categoriesMap;
-};
+    )
+);
