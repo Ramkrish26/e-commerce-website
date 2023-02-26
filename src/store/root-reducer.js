c1947ce0125008764a906153776a9a9f3bebbdcc
Ramkrish26git config user.name Ramkrish26 <ramakrishnan2629@gmail.com>
2023-02-26 11:39:19 +0530

    Migrating Cart Context to Redux

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 
 import { userReducer } from "./user/user.reducer";
 import { categoriesReducer } from "./categories/category.reducer";
+import { cartReducer } from "./cart/cart.reducer";
 
 //root-reducer (Combination of all reducers)
 export const rootReducer = combineReducers({
     user: userReducer,
     categories: categoriesReducer,
+    cart:cartReducer,
 })
